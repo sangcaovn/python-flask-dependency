7fc208fe930acac0c01c1cf7ae4e45691e0cc69f
Nguyen Thinh <nbthinh94@gmail.com>
2022-03-25 14:45:04 +0700

    try catch

--- wsgi.py
+++ wsgi.py
@@ -17,21 +17,24 @@ app = Flask(__name__)
 
 
 @inject
-@app.route('/data',methods=["POST"])
+@app.route('/data', methods=["POST"])
 def get_data(service: Service):
-    print(f"Service instance is {service}")  # We want to see the object that gets created
+    # We want to see the object that gets created
+    print(f"Service instance is {service}")
     return service.get_data()
 
 
 @inject
 @app.route('/index')
 def get_data_index(service: Service):
-    print(f"Service instance is {service}")  # We want to see the object that gets created
+    # We want to see the object that gets created
+    print(f"Service instance is {service}")
     return render_template(
         "index.html",
         data=service.get_data(),
         demo="Hello"
     )
+
 
 @app.route('/demo-render-html')
 def demo_render_html(service: Service):
@@ -44,14 +47,17 @@ def demo_render_html(service: Service):
         response=response
     )
 
+
 @app.route('/weather-info')
 def weather_info(service: Service):
-    response = requests.get("https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1").json()
+    response = requests.get(
+        "https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1").json()
     print(response)
     return render_template(
         "weather-info.html",
         weather_loop=response["list"]
     )
+
 
 @app.route('/weather-info-2')
 def weather_info2(service: Service):
@@ -70,6 +76,7 @@ def weather_info2(service: Service):
         main=main
     )
 
+
 @app.route('/weather_use_aio')
 def weather_use_aiohttp(service: Service):
     response = asyncio.run(service.call_api_use_io_service())
@@ -86,6 +93,7 @@ def weather_use_aiohttp(service: Service):
         main=main
     )
 
+
 @app.route('/demo-form')
 def demo_form(service: Service):
     return render_template(
@@ -96,13 +104,11 @@ def demo_form(service: Service):
 @app.route('/submit-form', methods=["POST"])
 def submit_form_handle(service: Service):
     data = request.form.to_dict(flat=False)
-
-    print ("data form submit >>>",data)
-
-    lst_data=None
+    print("data form submit >>>", data)
+    lst_data = None
     check = os.path.exists('./user-info/user_info.json')
     if(check):
-        lst_data=asyncio.run(service.read_file())
+        lst_data = asyncio.run(service.read_file())
         try:
             lst_data = json.loads(lst_data)
         except:
@@ -111,19 +117,31 @@ def submit_form_handle(service: Service):
         #     lst_data = json.loads(lst_data)
         # else:
         #     lst_data = []
-    print("check lst_data = ", lst_data)
-    print("check type(lst_data) = ", type(lst_data))
-    # print("check data = ", data)
     lst_data.append(data)
     asyncio.run(service.write_json_file(json.dumps(lst_data)))
     list_render = asyncio.run(service.read_file())
     list_render = json.loads(list_render)
-    print("list_render = ", list_render)
-    print(type(list_render))
     return render_template(
         "list_user.html",
         list_render=list_render
     )
+
+
+@app.route('/submit-form', methods=["GET"])
+def render_list_user(service: Service):
+    list_render = asyncio.run(service.read_file())
+    try:
+        list_render = json.loads(list_render)
+        return render_template(
+            "list_user.html",
+            list_render=list_render
+        )
+    except:
+        return render_template(
+            "index.html",
+            data=service.get_data(),
+            demo="Hello"
+        )
 
 
 # Setup Flask Injector, this has to happen AFTER routes are added
