c3bccc0c718d58aff076a12a1e013099fd3fc3b5
quoc-cuong <cgth@git.com>
2022-03-25 13:55:20 +0700

    CRUD with processing files

--- services/service.py
+++ services/service.py
@@ -1,6 +1,9 @@
 from select import select
 from injector import inject
 import asyncio
+import json
+import os
+import uuid
 
 from db.database import DatabaseBase
 
@@ -19,5 +22,96 @@ class Service:
     def get_general_weather_data(self):
         return asyncio.run(self.db.get_general_weather_data())
 
-    def get_git_data():
-        pass
+    @staticmethod
+    def parse_into_json_obj(filename):
+        with open(filename, 'r') as openfile:
+        # Reading from json file
+            return json.load(openfile)
+
+    def dump_into_json_file(self, json_obj):
+        """If option is 1 we use dump otherwise use dumps"""
+        dir_name = './user-info/'
+
+        complete_path = Service.get_complete_path()
+        # os.makedirs(complete_path)
+        if not os.path.isdir(dir_name):
+            os.mkdir(dir_name)
+
+        info = Service.parse_into_json_obj(complete_path)
+        print(f"\nExisting data: {info}, with type {type(info)}\n")
+
+        if len(info["list"]) > 0:
+            info['list'].append(json_obj)
+        else:
+            info = {}
+            info['list'] = [json_obj]
+
+        if json_obj['uuid'] == '':
+            user_uuid = str(uuid.uuid4()) 
+            json_obj['uuid'] = user_uuid
+            json_object = json.dumps(info, indent = 4)
+            with open(complete_path, "w") as outfile:
+                outfile.write(json_object)
+            print(f'\nNew UUID = {user_uuid}, create a new record!')
+        else:
+            # update_user(user_uuid)
+            pass
+
+    @staticmethod
+    def get_users():
+        dir_name = './user-info/'
+        filename = 'user.json'
+        complete_path = os.path.join(dir_name, filename)
+
+        return Service.parse_into_json_obj(complete_path)
+
+    @staticmethod
+    def get_complete_path():
+        dir_name = './user-info/'
+        filename = 'user.json'
+        complete_path = os.path.join(dir_name, filename)
+
+        return complete_path
+
+    @staticmethod
+    def get_user(uuid):
+        list_users = Service.parse_into_json_obj(Service.get_complete_path())
+        for user in list_users['list']:
+            if user['uuid'] == uuid:
+                return user
+        
+        return None
+
+    def update(self, uuid, new_data):
+        list_users = Service.parse_into_json_obj(Service.get_complete_path())
+        pos = 0
+        for user in list_users['list']:
+            if user['uuid'] == uuid:
+                user = new_data
+
+                print(f'\nNew user data: {user}')
+                # self.dump_into_json_file(list_users)
+                list_users['list'][pos] = user
+                list_users = json.dumps(list_users, indent = 4)
+                with open(Service.get_complete_path(), "w") as outfile:
+                    outfile.write(list_users)
+                return True
+            pos += 1
+
+        return False
+
+    def delete(self, uuid):
+        list_users = Service.parse_into_json_obj(Service.get_complete_path())
+        pos = 0
+        for user in list_users['list']:
+            if user['uuid'] == uuid:
+                print(f'\nNew user data: {user}')
+                # self.dump_into_json_file(list_users)
+                del list_users['list'][pos]
+                list_users = json.dumps(list_users, indent = 4)
+                with open(Service.get_complete_path(), "w") as outfile:
+                    outfile.write(list_users)
+                return True
+            pos += 1
+
+        return False
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_injector import FlaskInjector
 from injector import inject
+from flask import redirect, url_for
 
-from flask import render_template
+from flask import render_template, request
 
 from services.service import Service
 from dependencies import configure
@@ -42,6 +43,73 @@ def get_general_data_weather(service: Service):
     return render_template(
         "general_weather.html",
         data=service.get_general_weather_data(),
+    )
+
+@inject
+@app.route('/user-info', methods=['GET', 'POST'])
+def submit_user_form(service: Service):
+    print(f"Service instance is {service}")  # We want to see the object that gets created
+    if request.method == "POST":
+        uuid = request.form['uuid']
+        first_name = request.form['fname']
+        last_name = request.form['lname']
+        email = request.form['email']
+        address = request.form['address']
+        phone_number = request.form['phone']
+
+        print("\nUUID is: ", uuid)
+        print("\nFirst name is: ", first_name)
+        print("\nLast name is: ", last_name)
+        print("\nEmail is: ", email)
+        print("\nAddress is: ", address)
+        print("\nPhone Number: ", phone_number)
+
+        dict_form = request.form.to_dict()
+        print("Request: ", dict_form, " and its type: ", type(dict_form))
+        print("JSON Format: ", dict_form)
+
+        service.dump_into_json_file(dict_form)
+        return redirect(url_for("get_users", users=Service.get_users()))
+    
+    return render_template(
+        "user_form.html"
+    )
+
+@inject
+@app.route('/user-info/<uuid>', methods=['GET', 'POST'])
+def edit_user_form(service: Service, uuid):
+    if request.method == "POST":
+        dic_form = request.form.to_dict()
+        update_success = service.update(uuid, dic_form)
+        if update_success:
+            print("Update user successfully!")
+            return redirect(url_for("get_users", users=Service.get_users()))
+        else:
+            print('Update user failed!')
+
+    print("\nReceived UUID: ", uuid)
+    user_data = service.get_user(uuid)
+
+    return render_template(
+        "edit_user.html", user_data = user_data
+    )
+
+@inject
+@app.route('/delete/user/<uuid>', methods=['GET'])
+def remove_user(service: Service, uuid):
+    del_sucess = service.delete(uuid)
+    if del_sucess:
+        return redirect(url_for("get_users", users=Service.get_users()))
+    else:
+        print("Delete user failed!")
+
+@inject
+@app.route('/users')
+def get_users(service: Service):
+    print(f"Service instance is {service}")  # We want to see the object that gets created
+    return render_template(
+        "users.html",
+        users=Service.get_users(),
     )
 
 # Setup Flask Injector, this has to happen AFTER routes are added
